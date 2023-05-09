0a261368292703cdd3e4c727f327be337a9145f2
Basir Khan <khanbasir5555@gmail.com>
2023-05-09 15:10:45 +0530

    big issue fixed

--- backend/middleware/getuser.js
+++ backend/middleware/getuser.js
@@ -1,16 +1,17 @@
 const jwt = require("jsonwebtoken");
-const Jwt_Secret = "inotebookBasir#$#@!@#$";
+require("dotenv").config({path:"./.env"});
+const Jwt_Secret = process.env.Jwt_Secret;
 const fetchuser =(req,res,next)=>{
 const token =req.header('auth-token');
 if(!token){
-    res.status(401).send({error:"Please Authrnticate using a valid token"})
+    res.status(401).send({error:"Please Authrnticate using a valid token1"})
 }
 try{
 const data=jwt.verify(token,Jwt_Secret);
 req.user=data.user;
 next()
 }catch(err){
-    res.status(401).send({err:"Please Authrnticate using a valid token"})
+    res.status(401).send({err:"Please Authrnticate using a valid token2"})
 }
 }
 module.exports=fetchuser
